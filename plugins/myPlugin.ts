0c707a80a94aabda5b41e99ac7171bfd7abd586b
akh-ild <ildar.akhmadeyev@gmail.com>
2025-02-09 22:12:38 +0500

    hook

--- plugins/myPlugin.ts
+++ plugins/myPlugin.ts
@@ -1,9 +1,13 @@
 import { defineNuxtPlugin } from "nuxt/app";
 
-export default defineNuxtPlugin(() => {
-  return {
+export default defineNuxtPlugin((nuxtApp) => {
+  nuxtApp.hook('app:created', () => {
+    console.log('created');
+  });
+
+  /* return {
     provide: {
       sayHallo: (msg: string) => console.log(`Hallo ${msg}`),
     },
-  };
+  }; */
 })
