21b5d9d3d155144778d887ba35440671451791f2
akh-ild <ildar.akhmadeyev@gmail.com>
2025-02-11 18:51:17 +0500

    customization

--- components/Tailwind/index.vue
+++ components/Tailwind/index.vue
@@ -2,10 +2,14 @@
 </script>
 
 <template>
-  <div class="bg-green-600 text-white border-8 border-yellow-400">
+  <div class="bg-[#bada55] text-white border-8 border-yellow-400">
     Hello world
   </div>
-  <div class="text-customColor-100">
+  <div class="text-customColor-100 mb-5">
     Custom color
+  </div>
+
+  <div class="text-xs sm:text-base md:text-lg lg:text-3xl">
+    Long sentence
   </div>
 </template>
